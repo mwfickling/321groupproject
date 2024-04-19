5680184f6b1ecbd5dae283828fe5287d9668d418
lblayton <lblayton@crimson.ua.edu>
2024-04-18 19:37:57 -0500

    Add and Delete Recipe Database

---
+++ api/database/AddRecipe.cs
@@ -0,0 +1,33 @@
+using MySQL.Data.MySqlClient;
+using api.Models;
+
+namespace api.Database{
+    public class AddRecipe{
+        public void AddARecipe(Recipe newRecipe){
+            DatabaseConnection myConnection = new DatabaseConnection();
+
+            using var con = myConnection.GetPublicConnection();
+            con.Open();
+
+            //Insert the new recipe into the table
+            string stm = @"insert into recipes(recipeID, recipeName, cuisineType, instructions, numofServings, prepTime, cookTime, totalTime, recipeIMG, ingredientID) values(@recipeID, @recipeName, @cuisineType, @instructions, @numofServings, @prepTime, @cookTime, @totalTime, @recipeIMG, @ingredientID);"
+
+            using var cmd =  new MySqlCommand(stm, con);
+
+            //Parameters
+            cmd.Parameters.AddWithValue("@recipeID", newRecipe.recipeID);
+            cmd.Parameters.AddWithValue("@recipeName", newRecipe.recipeName);
+            cmd.Parameters.AddWithValue("@cuisineType", newRecipe.cuisineType);
+            cmd.Parameters.AddWithValue("@instructions", newRecipe.instructions);
+            cmd.Parameters.AddWithValue("@numofServings", newRecipe.numofServings);
+            cmd.Parameters.AddWithValue("@prepTime", newRecipe.prepTime);
+            cmd.Parameters.AddWithValue("@cookTime", newRecipe.cookTime);
+            cmd.Parameters.AddWithValue("@totalTime", newRecipe.totalTime);
+            cmd.Parameters.AddWithValue("@recipeIMG", newRecipe.recipeIMG);
+            cmd.Parameters.AddWithValue("@ingredientID", newRecipe.ingredientID);
+
+            cmd.Prepare();
+            cmd.ExecuteNonQuery();
+        }
+    }
+}
---
+++ api/database/DeleteRecipe.cs
@@ -0,0 +1,24 @@
+using MySQL.Data.MySqlClient;
+using api.Models;
+
+namespace api.Database{
+    public class DeleteRecipe{
+        public void DeleteAMovie(int id){
+            DatabaseConnection myConnection = new DatabaseConnection();
+
+            using var con = myConnection.GetPublicConnection();
+            con.Open();
+
+            // Delete movie with matching id from the table
+            // update and set delete in table to true 
+            string stm = @"update recipes set deleteRecipe = true where recipeID=@recipeID;";
+
+            using var cmd = new MySqlCommand(stm, con);
+
+            cmd.Parameters.AddWithValue("@recipeID", id);
+
+            cmd.Prepare();
+            cmd.ExecuteNonQuery();
+        }
+    }
+}
