8e59be4904f631db4bdc416e06cb65363679ea6f
Wishka Syed <wishkasyed@Wishkas-MacBook-Air.local>
2024-04-18 19:19:25 -0500

    backend

--- api/model/Recipe.cs
+++ api/model/Recipe.cs
@@ -12,7 +12,7 @@ namespace api.Models
         public string prepTime { get; set; }
         public string cookTime { get; set; }
         public string totalTime { get; set; }
-        public byte[] recipeIMG { get; set; }
+        public string recipeIMG { get; set; }
         public int ingredientID { get; set; }
         public bool deleteRecipe { get; set; }
     }
